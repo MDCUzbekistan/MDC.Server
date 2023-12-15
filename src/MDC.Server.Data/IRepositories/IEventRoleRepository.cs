c03cb2fcd6a34272ca84ad7341fb3fd802e4017c
Mukhammadzokir <139150511+MuxammadZokir@users.noreply.github.com>
2023-12-15 23:37:22 +0500

    Add: IUserEventRepository and IEventRoleRepository

---
+++ src/MDC.Server.Data/IRepositories/IEventRoleRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDC.Server.Data.IRepositories
+{
+    internal class IEventRoleRepository
+    {
+    }
+}
