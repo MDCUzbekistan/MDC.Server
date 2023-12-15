1495e88aaa78251c72ad5464fdb0e3e820274fcb
Ilhomjon-Azimov <ilhomjonazimov777@gmail.com>
2023-12-15 17:26:11 +0500

    Add:SpeakerDetailsDto

---
+++ src/MDC.Server.Service/DTOs/SpeakerDetails/SpeakerDetailsForUpdateDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDC.Server.Service.DTOs.SpeakerDetails
+{
+    public class SpeakerDetailsForUpdateDto
+    {
+        public string Company { get; set; }
+        public string Position { get; set; }
+        public string SpeechImage { get; set; }
+        public short ExperienceYear { get; set; }
+    }
+}
