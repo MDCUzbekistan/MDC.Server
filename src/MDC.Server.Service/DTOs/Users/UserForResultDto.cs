ef5b801b5a28be947cead5be79ec3aa362b75433
Khurshid <xurshidyoldoshev4@gmail.com>
2023-12-15 23:17:44 +0500

    Update UserForResultDto.cs

--- src/MDC.Server.Service/DTOs/Users/UserForResultDto.cs
+++ src/MDC.Server.Service/DTOs/Users/UserForResultDto.cs
@@ -7,6 +7,5 @@ public class UserForResultDto
     public string LastName { get; set; }
     public string Email { get; set; }
     public string PhoneNumber { get; set; }
-    public string Password { get; set; }
     public DateTime? DateOfBirth { get; set; }
 }
