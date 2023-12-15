47a2bbf58c462dce435020359db6ed9c0486ac13
Ilhomjon-Azimov <ilhomjonazimov777@gmail.com>
2023-12-16 03:19:49 +0500

    Add:Mapper

--- src/MDC.Server.Service/Mappings/MappingProfile.cs
+++ src/MDC.Server.Service/Mappings/MappingProfile.cs
@@ -1,10 +1,8 @@
 ﻿using AutoMapper;
 using MDC.Server.Domain.Entities.Users;
-<<<<<<< HEAD
 using MDC.Server.Service.DTOs.SpeakerDetails;
-=======
 using MDC.Server.Service.DTOs.Users;
->>>>>>> 15ba56d1649424411ce562f27f2d01944890147d
+
 
 namespace MDC.Server.Service.Mappings;
 
@@ -12,13 +10,12 @@ public class MappingProfile : Profile
 {
     public MappingProfile()
     {
-<<<<<<< HEAD
-=======
+
         #region
         CreateMap<User, UserForCreationDto>().ReverseMap();
         CreateMap<User, UserForUpdateDto>().ReverseMap();
         CreateMap<User, UserForResultDto>().ReverseMap();
         #endregion
->>>>>>> 15ba56d1649424411ce562f27f2d01944890147d
+
     }
 }
