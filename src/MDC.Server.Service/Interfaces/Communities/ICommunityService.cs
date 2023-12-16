fc4f597f83fdbbc9897b20708be62118883d5ded
Dilshodov01 <dilshodovziyodillo@gmail.com>
2023-12-16 23:44:11 +0500

    CommunityDTOs Added

---
+++ src/MDC.Server.Service/Interfaces/Communities/ICommunityService.cs
@@ -0,0 +1,12 @@
+﻿using MDC.Server.Service.DTOs.Community;
+
+namespace MDC.Server.Service.Interfaces.Communities;
+
+public  interface ICommunityService
+{
+    public Task<bool> DeleteAsync(long Id);
+    public Task<CommunityForResultDto> RetruveByIdAsync(long Id);
+    public Task<IEnumerable<CommunityForResultDto>> RetruveAllAsync();
+    public Task<CommunityForResultDto> UpdateAsync(CommunityForUpdateDto Update);
+    public Task<CommunityForResultDto> CreateAsync(CommunityForCreationDto community);
+}
