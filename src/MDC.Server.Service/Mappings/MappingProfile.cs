9abd59d630a34c415e1181c232e895bcce78b7cb
Dilafruza <dilafruzjraboyeva@gmail.com>
2023-12-16 18:14:13 +0500

    Add: LanguageService and Update: ServiceExtention.

--- src/MDC.Server.Service/Mappings/MappingProfile.cs
+++ src/MDC.Server.Service/Mappings/MappingProfile.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
+using MDC.Server.Domain.Entities.References;
 using MDC.Server.Domain.Entities.Users;
+using MDC.Server.Service.DTOs.Languages;
 using MDC.Server.Service.DTOs.Users;
 
 namespace MDC.Server.Service.Mappings;
@@ -9,9 +11,16 @@ public class MappingProfile : Profile
     public MappingProfile()
     {
         #region
+        //Users
         CreateMap<User, UserForCreationDto>().ReverseMap();
         CreateMap<User, UserForUpdateDto>().ReverseMap();
         CreateMap<User, UserForResultDto>().ReverseMap();
+
+        //Languages
+        CreateMap<Language, LanguageForCreationDto>().ReverseMap();
+        CreateMap<Language, LanguageForUpdateDto>().ReverseMap();
+        CreateMap<Language, LanguageForResultDto>().ReverseMap();
+
         #endregion
     }
 }
