72fb3290c1947326ea73f571d201c6b49688d340
Dilshodov01 <dilshodovziyodillo@gmail.com>
2023-12-17 00:15:19 +0500

    CommunityController Added

---
+++ src/MDC.Server.Api/Controllers/Communities/CommunityController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MDC.Server.Service.DTOs.Community;
+using MDC.Server.Service.Interfaces.Communities;
+
+namespace MDC.Server.Api.Controllers.Communities
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommunityController : ControllerBase
+    {
+        private readonly ICommunityService _communityService;
+
+        public CommunityController(ICommunityService communityService)
+        {
+            _communityService = communityService;
+        }
+
+        [HttpGet("community-id")]
+        public async Task<IActionResult> GetByIdAsync([FromRoute(Name = "community-id")] long Id)
+            =>Ok(await _communityService.RetruveByIdAsync(Id));
+
+        [HttpGet]
+        public async Task<IActionResult>GeAllAsync()
+            =>Ok(await _communityService.RetruveAllAsync());
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync([FromBody]CommunityForCreationDto community)
+            =>Ok(await _communityService.CreateAsync(community));
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateAsync([FromBody]CommunityForUpdateDto community)
+            =>Ok(await _communityService.UpdateAsync(community));
+        
+    }
+}
