4af3d7cdeef08c805150be5c3da54a52b088a37c
Shamsiddin_Dev <119076053+Shamsiddin13@users.noreply.github.com>
2023-12-18 23:15:37 +0500

    Added: DTOs for UserDetailService

---
+++ src/MDC.Server.Service/DTOs/UserDetails/UserDetailForCreationDto.cs
@@ -0,0 +1,11 @@
+﻿using MDC.Server.Domain.Entities.Users;
+using Microsoft.AspNetCore.Http;
+
+namespace MDC.Server.Service.DTOs.UserDetails;
+
+public class UserDetailForCreationDto
+{
+    public long UserId { get; set; }
+    public IFormFile Image { get; set; }
+    public IFormFile Resume { get; set; }
+}
