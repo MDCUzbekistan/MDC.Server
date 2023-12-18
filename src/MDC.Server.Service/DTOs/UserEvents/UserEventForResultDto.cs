c5891562f31a65fa5cd4964ee3c93c7bc2e6aa7b
Mukhammadzokir <139150511+MuxammadZokir@users.noreply.github.com>
2023-12-18 15:05:38 +0500

    Updated: UserEventDtos

--- src/MDC.Server.Service/DTOs/UserEvents/UserEventForResultDto.cs
+++ src/MDC.Server.Service/DTOs/UserEvents/UserEventForResultDto.cs
@@ -1,12 +1,13 @@
-﻿using MDC.Server.Domain.Entities.Users;
-using MDC.Server.Domain.Entities.Events;
+﻿using MDC.Server.Service.DTOs.Users;
+using MDC.Server.Service.DTOs.Events;
+using MDC.Server.Service.DTOs.EventRoles;
 
 namespace MDC.Server.Service.DTOs.UserEvents;
 
 public class UserEventForResultDto
 {
     public long Id { get; set; }
-    public User User { get; set; }
-    public Event Event { get; set; }
-    public EventRole Role { get; set; }
+    public UserForCreationDto User { get; set; }
+    public EventForCreationDto Event { get; set; }
+    public EventRoleForCreationDto Role { get; set; }
 }
